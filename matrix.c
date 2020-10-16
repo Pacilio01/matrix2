ca0f79bfdca2962ab6e5d03feb3b3f0a4197f8ff
Carmine Pacilio <carmine@Carmines-MacBook-Pro.local>
2020-10-16 12:17:02 +0200

    commit

--- matrix.c
+++ matrix.c
@@ -3,41 +3,64 @@
 #define DIM 100
 typedef enum { true, false } bool;
 
-float coeff(int a, int b){
-    int x;
-    x = -a/b;
+float coeff(float a, float b){
+    float x;
+    x = (-1)*(a/b);
     return x;
 }
 
 void riduci(float matrix[DIM][DIM], int Nrighe, int Ncolonne){
-    int pos;
+    float cf = 0;
+    float tmp;
     bool pivot = false;
     int stato = 0;
+    int posSost = 0;
 
-    for (int i = 0; i < Ncolonne; i++) {
-        while (pivot == false){
-            if (matrix[stato][i] != 0) {
-                for (int j = stato+1; j < Nrighe; j++) {
-                    if (matrix[j][i] != 0) {
-                        for (int z = 0; z < Nrighe; z++) {
-                            matrix[j][z] += coeff(matrix[i][z], matrix[stato][i]);
-                            pivot = true;
-                        }
-                    } else {
-                        pivot = true;
-                    } 
+    for (int i = 0; i < Nrighe-1; i++) {
+        if (matrix[stato][i] != 0 && stato < Nrighe) {
+            for (int j = stato+1; j < Nrighe; j++) {
+                if (matrix[j][i] != 0) {
+                    pivot = false;
+                    cf = coeff(matrix[j][i], matrix[stato][i]);
+                    for (int z = 0; z < Ncolonne; z++) {
+                        matrix[j][z] += cf*matrix[stato][z];
+                    }
+                    pivot = true;
+                } else {
+                    pivot = true;
                 }
-                
             }
-            
+        } else {
+            for (int rSotto = Nrighe; rSotto > i+1; rSotto--) {
+                if (matrix[rSotto][i] != 0) {
+                    posSost = rSotto;
+                    break;
+                }
+            }
+
+            for (int indiceCol = 0; indiceCol < Ncolonne; indiceCol++) {
+                tmp = matrix[stato][indiceCol];
+                matrix[stato][indiceCol] = matrix[posSost][indiceCol];
+                matrix[posSost][indiceCol] = tmp;
+            }
+            stato--;
+            i--;
         }
-        pivot = false;
+        stato++;
+        for (int i = 0; i < Nrighe; i++) {
+        for (int j = 0; j < Ncolonne; j++) {
+            printf("| %6.3f ", matrix[i][j]);
+        }
+        printf(" |\n");
+    }
+    printf("\n");
     }
 }
 
 int main(void) {
     int Nrighe, Ncolonne;
     float matrix[DIM][DIM];
+    float ridotta[DIM][DIM];
 
     printf("Inserire numero di righe: ");
     scanf("%d", &Nrighe);
@@ -50,18 +73,28 @@ int main(void) {
         for (int j = 0; j < Ncolonne; j++) {
             printf("Inserire valore %d,%d: ", i, j);
             scanf("%f", &matrix[i][j]);
+            ridotta[i][j] = matrix[i][j];
             printf("\n");
         }
     }
 
-    riduci(matrix, Nrighe, Ncolonne);
-
     for (int i = 0; i < Nrighe; i++) {
         for (int j = 0; j < Ncolonne; j++) {
-            printf("| %f.1 |", matrix[i][j]);
+            printf("| %6.3f |", matrix[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
     
+    riduci(ridotta, Nrighe, Ncolonne);
+
+    for (int i = 0; i < Nrighe; i++) {
+        for (int j = 0; j < Ncolonne; j++) {
+            printf("| %6.3f ", ridotta[i][j]);
+        }
+        printf("|\n");
+    }
+    printf("\n");
+
     return 0;
 }
